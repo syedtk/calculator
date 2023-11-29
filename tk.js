18ac8432c0b6474b90e08f9dcd4d6bb3d94cd8d5
01723710909 <137289906+01723710909@users.noreply.github.com>
2023-11-30 01:07:07 +0600

    first commit

---
+++ tk.js
@@ -0,0 +1,28 @@
+let scrn = document.getElementById('screen');
+
+function input(x){
+    scrn.value = scrn.value + x
+}
+function allClear() {
+    scrn.value= ''
+}
+ function del() {
+    scrn.value = scrn.value.slice(0,-1)
+ }
+
+function calc(){
+    try{
+        scrn.value = eval(scrn.value)
+    }
+    catch{
+
+        scrn.value='Error'
+    }
+  
+}
+
+
+
+ 
+
+
